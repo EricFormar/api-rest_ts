0bd85bb7f8cc66c7bb191a45c084064e39677661
@emena <menaericdaniel@gmail.com>
2025-06-13 12:44:05 -0300

    refactor: use brand model in mock brand creation

--- src/tests/mocks/brand.mock.ts
+++ src/tests/mocks/brand.mock.ts
@@ -1,53 +1,18 @@
-import { Model, QueryInterface } from "sequelize";
-import { DataTypesTest, queryInterface, sequelizeInstance } from "../setup";
+import { QueryInterface } from "sequelize";
+import { DataTypesTest, queryInterface } from "../setup";
 import migration from "../../database/migrations/20250607110250-brand";
+import Brand  from "../../database/models/brand";
 
-export interface IBrand extends Model {
-    id: number;
-    name: string;
-    image: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt?: Date | null;
-  }
 export const getBrandMock = async () => {
 
     await migration.up(queryInterface as QueryInterface, DataTypesTest);
-    
-    const Brands = sequelizeInstance.define<IBrand>(
-        "Brand",
-        {
-          id: {
-            type: DataTypesTest.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-          },
-          name: {
-            type: DataTypesTest.STRING,
-          },
-          image: {
-            type: DataTypesTest.STRING,
-          },
-          createdAt: {
-            type: DataTypesTest.DATE,
-            allowNull: false,
-          },
-          updatedAt: {
-            type: DataTypesTest.DATE,
-            allowNull: false,
-          },
-          deletedAt: {
-            type: DataTypesTest.DATE,
-            allowNull: true,
-          },
-        },
-        { timestamps: true, paranoid: true }
-      );
   
-      const newBrand = await Brands.create({
+      const newBrand = await Brand.create({
+        id: 1,
         name: "Test Brand",
         image: "test.jpg",
+        createdAt : new Date,
+        updatedAt : new Date
       });
 
       return newBrand;                        
